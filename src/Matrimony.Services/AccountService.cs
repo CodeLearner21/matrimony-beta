5897cffd524b0d6b5d8cd3e90380789297b99f6c
vipul <vipul@lthonline.com>
2019-09-26 15:05:58 +0530

    Account service added
    - Dependency inject service register
    - Account controller added
    - Account service added
    - User repository added
    - Database context updated
    - AppUser updated
    - Automapper profile created

--- src/Matrimony.Services/AccountService.cs
+++ src/Matrimony.Services/AccountService.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using AutoMapper;
+using Matrimony.Database.Entities;
+using Matrimony.Database.Repository.Interfaces;
+using Matrimony.Dtos;
+using Matrimony.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Matrimony.Services
 {
-    class AccountService
+    public class AccountService : IAccountService
     {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public AccountService(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserRegisterDto userRegister)
+        {
+            try
+            {
+                var appUser = _mapper.Map<AppUser>(userRegister);
+                var result = await _userRepository.Create(appUser, userRegister.Password);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
--- src/Matrimony.WebAPI/V1/Controllers/AccountController.cs
+++ src/Matrimony.WebAPI/V1/Controllers/AccountController.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Matrimony.Dtos;
+using Matrimony.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Matrimony.WebAPI.V1.Controllers
 {
-    [Route("api/[controller]")]
+    [Authorize]
+    [Route("api/V{v:apiVersion}/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly IAccountService _accountService;
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("Register")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Register(UserRegisterDto userRegister)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var result = await _accountService.RegisterUser(userRegister);
+            if(!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            return Ok();
+        }
     }
 }
